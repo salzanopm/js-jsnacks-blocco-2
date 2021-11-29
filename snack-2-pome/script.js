053643c3c3b9b604dc90397bbe145afef5b8efb6
salzanopm <salzanopm@email.com>
2021-11-29 18:07:29 +0100

    finished

--- snack-2-pome/script.js
+++ snack-2-pome/script.js
@@ -16,31 +16,17 @@ console.log(`Cognomi`, surnameArray);
 // array nomi e cognomi casuali
 const casualName = [];
 
-// dichiaro variabile nome random
-let nameRandom;
 for (let i = 0; i < nameArray.length; i++) {
-    console.log(nameArray);
-    let thisName = nameArray[i];
-    console.log(thisName);
+    
     // funzione scelta random nome
-    thisName = nameArray[Math.floor(Math.random() * ((nameArray.length - 1) + 1) - 0)];
-    console.log(thisName);
-    nameRandom = thisName;
-}
-
-// dichiaro variabile cognome random
-let randomLastName;
-for (let i = 0; i < surnameArray.length; i++) {
-    console.log(surnameArray);
-    let thisLastName = surnameArray[i];
-    console.log(thisLastName);
+    const thisName = nameArray[Math.floor(Math.random() * ((nameArray.length - 1 )+ 1) - 0)];
+   
     // funzione scelta random nome
-    thisLastName = surnameArray[Math.floor(Math.random() * ((surnameArray.length -1) + 1) - 0)];
-    console.log(thisLastName);
-    randomLastName = thisLastName;
-}
-
-// nome e cognome stampo
-const guestName = nameRandom + ' ' + randomLastName;
+    const thisLastName = surnameArray[Math.floor(Math.random() * ((surnameArray.length -1 )+ 1) - 0)];
+   // nome e cognome stampo
+const guestName = thisName + ' ' + thisLastName;
 casualName.push(guestName);
-console.log(`Invitati`, casualName);
+console.log(guestName)
+
+}
+
